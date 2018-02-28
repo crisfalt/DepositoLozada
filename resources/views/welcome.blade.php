68dc03daea25bceb25e0cc393013dade05b820c9
crisfalt <crisfalt@gmail.com>
2018-02-28 15:58:23 -0500

    PlantilaDefinida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,286 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('title','DepositoLozada | DashBoard')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('header-class')
+<div class="panel-header">
+    <canvas id="bigDashboardChart"></canvas>
+</div>
+@endsection
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+@section('contenido')
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card card-chart">
+            <div class="card-header">
+                <h5 class="card-category">Global Sales</h5>
+                <h4 class="card-title">Shipped Products</h4>
+                <div class="dropdown">
+                    <button type="button" class="btn btn-round btn-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown">
+                        <i class="now-ui-icons loader_gear"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item text-danger" href="#">Remove Data</a>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div>
+            <div class="card-body">
+                <div class="chart-area">
+                    <canvas id="lineChartExample"></canvas>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+    <div class="col-lg-4 col-md-6">
+        <div class="card card-chart">
+            <div class="card-header">
+                <h5 class="card-category">2018 Sales</h5>
+                <h4 class="card-title">All products</h4>
+                <div class="dropdown">
+                    <button type="button" class="btn btn-round btn-default dropdown-toggle btn-simple btn-icon no-caret" data-toggle="dropdown">
+                        <i class="now-ui-icons loader_gear"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item text-danger" href="#">Remove Data</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="chart-area">
+                    <canvas id="lineChartExampleWithNumbersAndGrid"></canvas>
+                </div>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="now-ui-icons arrows-1_refresh-69"></i> Just Updated
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-6">
+        <div class="card card-chart">
+            <div class="card-header">
+                <h5 class="card-category">Email Statistics</h5>
+                <h4 class="card-title">24 Hours Performance</h4>
+            </div>
+            <div class="card-body">
+                <div class="chart-area">
+                    <canvas id="barChartSimpleGradientsNumbers"></canvas>
+                </div>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="now-ui-icons ui-2_time-alarm"></i> Last 7 days
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+    <div class="card card-tasks">
+        <div class="card-header">
+        <h5 class="card-category">Backend development</h5>
+        <h4 class="card-title">Tasks</h4>
+        </div>
+        <div class="card-body">
+        <div class="table-full-width table-responsive">
+            <table class="table">
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" checked="">
+                                <span class="form-check-sign"></span>
+                            </label>
+                        </div>
+                    </td>
+
+                    <td class="text-left">Sign contract for "What are conference organizers afraid of?"</td>
+                    <td class="td-actions text-right">
+                        <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
+                            <i class="now-ui-icons ui-2_settings-90"></i>
+                        </button>
+                        <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
+                            <i class="now-ui-icons ui-1_simple-remove"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox">
+                                <span class="form-check-sign"></span>
+                            </label>
+                        </div>
+                    </td>
+
+                    <td class="text-left">Lines From Great Russian Literature? Or E-mails From My Boss?</td>
+                    <td class="td-actions text-right">
+                        <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
+                            <i class="now-ui-icons ui-2_settings-90"></i>
+                        </button>
+                        <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
+                            <i class="now-ui-icons ui-1_simple-remove"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" checked="">
+                                <span class="form-check-sign"></span>
+                            </label>
+                        </div>
+                    </td>
+
+                    <td class="text-left">Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
+                    </td>
+                    <td class="td-actions text-right">
+                        <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
+                            <i class="now-ui-icons ui-2_settings-90"></i>
+                        </button>
+                        <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
+                            <i class="now-ui-icons ui-1_simple-remove"></i>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        </div>
+        <div class="card-footer">
+        <hr>
+        <div class="stats">
+            <i class="now-ui-icons loader_refresh spin"></i> Updated 3 minutes ago
+        </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card">
+        <div class="card-header">
+            <h5 class="card-category">All Persons List</h5>
+            <h4 class="card-title"> Employees Stats</h4>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                <thead class=" text-primary">
+                    <th >
+                    Name
+                    </th>
+                    <th >
+                    Country
+                    </th>
+                    <th >
+                    City
+                    </th>
+                    <th  class="text-right" >
+                    Salary
+                    </th>
+                </thead>
+                <tbody>
+                    <tr >
+                    <td >
+                        Dakota Rice
+                    </td>
+                    <td >
+                        Niger
+                    </td>
+                    <td >
+                        Oud-Turnhout
+                    </td>
+                    <td  class="text-right" >
+                        $36,738
+                    </td>
+                    </tr>
+                    <tr >
+                    <td >
+                        Minerva Hooper
+                    </td>
+                    <td >
+                        Curaçao
+                    </td>
+                    <td >
+                        Sinaai-Waas
+                    </td>
+                    <td  class="text-right" >
+                        $23,789
+                    </td>
+                    </tr>
+                    <tr >
+                    <td >
+                        Sage Rodriguez
+                    </td>
+                    <td >
+                        Netherlands
+                    </td>
+                    <td >
+                        Baileux
+                    </td>
+                    <td  class="text-right" >
+                        $56,142
+                    </td>
+                    </tr>
+                    <tr >
+                    <td >
+                        Doris Greene
+                    </td>
+                    <td >
+                        Malawi
+                    </td>
+                    <td >
+                        Feldkirchen in Kärnten
+                    </td>
+                    <td  class="text-right" >
+                        $63,542
+                    </td>
+                    </tr>
+                    <tr >
+                    <td >
+                        Mason Porter
+                    </td>
+                    <td >
+                        Chile
+                    </td>
+                    <td >
+                        Gloucester
+                    </td>
+                    <td  class="text-right" >
+                        $78,615
+                    </td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function() {
+        // Javascript method's body can be found in assets/js/demos.js
+        demo.initDashboardPageCharts();
+    });
+</script>
+@endsection
